70b9b33d65832efc23e33b069bb14166262bc446
Hussein Martinez <husse.dev@gmail.com>
2024-09-27 02:24:28 +0700

    chore: added default children in meta object

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -4,17 +4,16 @@ import { Button } from './button';
 const meta = {
   title: 'Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
 } satisfies Meta;
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-  args: {
-    children: 'Primary',
-  },
-};
+export const Primary: Story = {};
 export const Secondary: Story = {
   args: {
     children: 'Secondary',
