e2e34575ece157371de16f1c26aad414decab1db
Hussein Martinez <husse.dev@gmail.com>
2024-09-27 03:31:29 +0700

    feat: secondary and destructive variants

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -13,9 +13,20 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
+
 export const Secondary: Story = {
   args: {
-    children: 'Secondary',
+    variant: 'secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
   },
 };
