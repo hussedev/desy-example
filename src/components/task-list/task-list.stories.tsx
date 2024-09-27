bab3e4351b4067e3525fe4cdfb2162796b6964c6
Hussein Martinez <husse.dev@gmail.com>
2024-09-27 15:32:49 +0700

    chore: use decorator for TaskListProvider

--- src/components/task-list/task-list.stories.tsx
+++ src/components/task-list/task-list.stories.tsx
@@ -1,9 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TaskList } from './task-list';
+import { TaskListProvider } from './task-list-context';
 
 const meta = {
   title: 'Components/TaskList',
   component: TaskList,
+  decorators: [
+    (Story, context) => {
+      console.log(context);
+      return (
+        <TaskListProvider
+          tasks={[
+            { id: '1', title: 'Task 1', completed: true },
+            { id: '2', title: 'Task 2', completed: false },
+            { id: '3', title: 'Task 3', completed: false },
+          ]}
+        >
+          <Story {...context} />
+        </TaskListProvider>
+      );
+    },
+  ],
 } as Meta<typeof TaskList>;
 
 export default meta;
