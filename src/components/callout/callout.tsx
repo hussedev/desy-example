a7d905754035504db4971710b005156219519f45
Hussein Martinez <husse.dev@gmail.com>
2024-09-27 14:12:38 +0700

    chore: changed callout style for visual testing

--- src/components/callout/callout.tsx
+++ src/components/callout/callout.tsx
@@ -6,7 +6,7 @@ export type CalloutProps = PropsWithChildren<CalloutVariants & { title: string }
 export const Callout = ({ children, title, variant }: CalloutProps) => {
   return (
     <div className={calloutVariants({ variant })}>
-      <h2 className="font-semibold">{title}</h2>
+      <h2 className="text-2xl font-semibold">{title}</h2>
       <p>{children}</p>
     </div>
   );
