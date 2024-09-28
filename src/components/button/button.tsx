c0473dafa4d20b5bbf8b94590293ba67aae17fd2
Hussein Martinez <husse.dev@gmail.com>
2024-09-29 00:05:22 +0700

    refactor: button component to render conditionally as a link

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,17 @@
-import { ComponentProps } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { buttonVariants, type ButtonVariants } from './button-variants';
+import clsx from 'clsx';
 
-export type ButtonProps = ComponentProps<'button'> & ButtonVariants;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & ButtonVariants & { href?: never };
+type AnchorProps = ComponentPropsWithoutRef<'a'> & ButtonVariants & { href?: string };
+type ButtonOrLinkProps = ButtonProps | AnchorProps;
+export const Button = ({ variant = 'primary', size = 'medium', ...props }: ButtonOrLinkProps) => {
+  const className = clsx(buttonVariants({ size, variant }));
 
-export const Button = ({
-  variant = 'primary',
-  size = 'medium',
-  className,
-  ...props
-}: ButtonProps) => {
-  return <button {...props} className={buttonVariants({ size, variant })} />;
+  if (props.href) {
+    return <a className={className} {...(props as ComponentPropsWithoutRef<'a'>)} />;
+  } else {
+    return <button className={className} {...(props as ComponentPropsWithoutRef<'button'>)} />;
+  }
 };
